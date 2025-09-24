34d694a4befe7f0af536c49c862ed260b3aec20e
Kadapah Imran <kadapaimran99@hotmail.com>
2025-09-24 14:33:12 +0530

    Update ProductController.java

--- src/main/java/com/klu/ecommerce/controller/ProductController.java
+++ src/main/java/com/klu/ecommerce/controller/ProductController.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*")
 public class ProductController {
 
     private final ProductRepository productRepository;
